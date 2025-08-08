ece038f286951d18edc9b8d67ffc3f74b5842784
Husain Khan <hk149335@gmail.com>
2025-08-09 00:18:04 +0530

    Add 03_input_output.cpp: cin and cout usage

--- 01_basics/02_variables.cpp
+++ 01_basics/02_variables.cpp
@@ -5,4 +5,5 @@ int main()
 {
     int length = 2 , breadth = 4;
     std::cout<<"Area is : "<<length*breadth<<std::endl;
+    return 0;
 }
---
+++ 01_basics/03_input_output.cpp
@@ -0,0 +1,11 @@
+// 03_input_output.cpp
+#include<iostream>
+
+int main()
+{
+	int num;
+	std::cout<<"Enter the number to print on console : ";
+	std::cin>>num;
+	std::cout<<"Given number is : "<<num<<std::endl;
+	return 0;
+}
