f96dfc29aafde427f886011ed9e7aeda7309d14c
Husain Khan <hk149335@gmail.com>
2025-08-09 00:47:32 +0530

    Add initialization code

--- 01_basics/02_variables.cpp
+++ 01_basics/02_variables.cpp
@@ -5,5 +5,12 @@ int main()
 {
     int length = 2 , breadth = 4;
     std::cout<<"Area is : "<<length*breadth<<std::endl;
+
+    // Direct initialization : initial value is provided inside parenthesis
+    int a (2);
+
+    // List-initialization: use of curly braces
+    int b {1};
+    int c = {b}; // copy-list initialization
     return 0;
 }
