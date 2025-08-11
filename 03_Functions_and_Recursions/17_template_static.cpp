ef52344bc0de30170d0df22d8c5cf41064a42049
Husain Khan <hk149335@gmail.com>
2025-08-11 07:49:29 +0530

    Add 17_template_static.cpp - function templates with modifiable static local variables

--- 03_Functions_and_Recursions/17_template_static.cpp
+++ 03_Functions_and_Recursions/17_template_static.cpp
@@ -1,3 +1,8 @@
+//When a static local variable is used in a function template, 
+//each function instantiated from that template will have a separate version of the static local variable. 
+//This is rarely a problem if the static local variable is const. 
+//But if the static local variable is one that is modified, the riesults may not be as expected.
+
 #include <iostream>
 
 // Here's a function template with a static local variable that is modified
